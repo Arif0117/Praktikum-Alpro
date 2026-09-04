cf3aac254f7cb78f79d90a4d23b95ba9180f76be
Arif0117 <muhariffff02@gmail.com>
2026-09-04 10:34:37 +0700

    Add files via upload

---
+++ 0040_MuhammadArif_Literal_Data.py
@@ -0,0 +1,33 @@
+#nama = "Arif" #string
+#umur = 18 #integer
+#TB = 165.2 #float
+
+#print("Nama:", nama)
+#print("Umur:", umur, "tahun")
+#print("Tinggi Badan:", TB, "cm")
+
+#data_str = "123" #memasukkan data string
+#data_int = int(data_str) #mengubah data string menjadi integer
+#print("data:", data_int, ", type:", type(data_int)) #output yang dikeluarkan
+
+#data_float = 45.67 #memasukkan data float
+#data_int = int(data_float) #mengubah data float menjadi string
+#print("data:", data_int, ", type:", type(data_int)) #output yang dikeluarkan
+
+#data_int = 89 #memasukkan data integer
+#data_float = float(data_int) #mengubah data integer menjadi float
+#print("data:", data_float, ", type:", type(data_float)) #output yang dikeluarkan
+
+#data_int = 89 #memasukkan data integer
+#data_str = str(data_int) #mengubah data integer menjadi string
+#print("data:", data_str, ", type:", type(data_str)) #output yang dikeluarkan
+
+
+#nama = input("Masukkan nama: ") #memasukkan data string
+#umur = int(input("Masukkan umur: ")) #memasukkan data integer
+#TB = float(input("Masukkan tinggi badan: ")) #memasukkan data float
+
+#print("/n--Data yang telah dimasukkan--")
+#print(f"Nama: {nama}")
+#print(f"Umur: {umur}")
+#print(f"Tinggi Badan: {TB}")
