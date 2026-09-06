4e47e7b5f81f21c71c08c7d64ebce93ab6d57875
Arif0117 <muhariffff02@gmail.com>
2026-09-06 22:54:22 +0700

    Add files via upload

---
+++ 0040_Operasi_Aritmatika_dan_Komperasi.py
@@ -0,0 +1,186 @@
+#Memasukkan variabel a dan b
+#a=9
+#b=10
+
+#operasi tambah +
+#hasil = a + b #Menjumlahkan variabel a dan b
+#print(a, "+", b, "=", hasil) #Mencetak hasil penjumlahan
+
+#Memasukkan variabel a dan b
+#a = 7
+#b = 1 
+
+#Operasi pengurangan
+#hasil = a - b #mengurangi variabel a dengan variabel b
+#print(a, "-", b, "=", hasil) #Mencetak hasil pengurangan
+
+#Memasukkan variabel a dan b
+#a = 6
+#b = 7
+
+#Operasi perkalian
+#hasil = a*b #melakukan perkalian variabel dari a dan b
+#print(a, "*", b, "=", hasil) # Mencetak hasil dari perkalian
+
+#Memasukkan variabel a dan b
+#a = 6
+#b = 2
+
+#Operasi pembagian
+#hasil = a / b #membagi variabel a dengan b
+#print(a, "/", b, "=", hasil) #mencetak hasil dari pembagian
+
+#memasukkan variabel a dan b
+#a = 4
+#b = 2
+
+#operasi eksponen (pangkat)
+#hasil = a ** b #melakukan operasi pangkat
+#print(a, "**",b "=", hasil) #mencetak hasil pangkat variabel a dan b
+
+#memasukkan variabel a dan b
+#a = 17
+#b = 5
+
+#Operasi moduls
+#hasil  = a // b #Melakukan operasi moduls
+#print(a, "//", b, "=", hasil) #Mencetak hasil pengoperasian
+
+#Memasukkan variabel a dan b
+#a = 17
+#b = 3
+
+#Operasi floor division
+#hasil = a % b #Pembagian bulat
+#print(a, "%", b, "=", hasil) #Mencetak hasil operasi
+
+#Program konversi celcius ke satuan lain
+
+#print("\nPROGRAM KONVERSI TEMPERATUR\n") #judul program
+
+#celcius = float(input('Masukan suhu dalam celscius : '))
+#print("Suhu adalah", celcius, "Celcius")
+
+# mengkonversi ke reamur
+#reamur = (4/5) * celcius
+#print("Suhu dalam Reamur:", reamur, "Reamur")
+
+# Mengkonversi ke Fahrenheit
+#fahrenheit = (9/5) * celcius + 32
+#print("Suhu dalam Fahrenheit:", fahrenheit, "Fahrenheit")
+
+# Mengkonversi ke Kelvin
+#kelvin = celcius + 273
+#print("Suhu dalam Kelvin:", kelvin, "Kelvin")
+
+# Operasi Komparasi
+# Setiap hasil dari operasi komparasi adalah Boolean
+# Operator: >, <, >=, <=, ==, !=, is, is not
+
+#a = 4
+#b = 2
+
+# Lebih besar dari >
+#print("=============== Lebih besar dari (>)")
+
+#hasil = a > 3
+#print(a, ">", 3, "=", hasil)
+
+#hasil = b > 3
+#print(b, ">", 3, "=", hasil)
+
+#hasil = b > 2
+#print(b, ">", 2, "=", hasil)
+
+# Kurang dari <
+#print("=============== Kurang dari (<)")
+
+#hasil = a < 3
+#print(a, "<", 3, "=", hasil)
+
+#hasil = b < 3
+#print(b, "<", 3, "=", hasil)
+
+#hasil = b < 2
+#print(b, "<", 2, "=", hasil)
+
+# Lebih besar dari atau sama dengan >=
+#print("=============== Lebih besar dari atau sama dengan (>=)")
+
+#hasil = a >= 3
+#print(a, ">=", 3, "=", hasil)
+
+#hasil = b >= 3
+#print(b, ">=", 3, "=", hasil)
+
+#hasil = b >= 2
+#print(b, ">=", 2, "=", hasil)
+
+# Kurang dari atau sama dengan <=
+#print("=============== Kurang dari atau sama dengan (<=)")
+
+#hasil = a <= 3
+#print(a, "<=", 3, "=", hasil)
+
+#hasil = b <= 3
+#print(b, "<=", 3, "=", hasil)
+
+#hasil = b <= 2
+#print(b, "<=", 2, "=", hasil)
+
+# Sama dengan ==
+#print("=============== Sama dengan (==)")
+
+#hasil = a == 4
+#print(a, "==", 4, "=", hasil)
+
+#hasil = b == 4
+#print(b, "==", 4, "=", hasil)
+
+# Tidak sama dengan !=
+#print("=============== Tidak sama dengan (!=)")
+
+#hasil = a != 4
+#print(a, "!=", 4, "=", hasil)
+
+#hasil = b != 4
+#print(b, "!=", 4, "=", hasil)
+
+# is sebagai komparasi identitas objek
+#x = 5
+#y = 5
+
+#hasil = x is y
+#print("x is y =", hasil)
+
+# is not sebagai komparasi identitas objek
+#x = 5
+#y = 6
+
+#hasil = x is not y
+#print("x is not y =", hasil)
+
+# a. Menghitung luas, volume, dan keliling balok
+
+#panjang = 12
+#lebar = 5
+#tinggi = 8
+
+# Luas permukaan balok
+#luas = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi)
+#print("\nLuas Permukaan Balok =", luas)
+
+# Volume balok
+#volume = panjang * lebar * tinggi
+#print("Volume Balok =", volume)
+
+# Keliling balok
+#keliling = 4 * (panjang + lebar + tinggi)
+
+#print("Keliling Balok =", keliling)
+
+# b. Apakah luas bangunan tersebut lebih luas dari 50?
+#print("Apakah luas bangunan lebih besar dari 50?", luas > 50)
+
+# c. Apakah volume bernilai 480
+#print("Apakah volume bangunan bernilai 480?", volume == 480)
